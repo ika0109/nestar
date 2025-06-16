40a974b92a4755205bf688712f5cf927cf8a229c
ika0109 <ikhboloy97@gmail.com>
2025-06-16 23:59:56 +0900

    fix: modify updateMember graphQl Api

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -1,11 +1,12 @@
 import { BadGatewayException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, ObjectId } from 'mongoose';
 import { Member } from '../../libs/dto/member/member';
 import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
 import { MemberStatus } from '../../libs/enums/member.enum';
 import { Message } from '../../libs/enums/common.enum';
 import { AuthService } from '../auth/auth.service';
+import { MemberUpdate } from '../../libs/dto/member/member.update';
 
 @Injectable()
 export class MemberService {
@@ -46,9 +47,21 @@ export class MemberService {
 		response.accsessToken = await this.authService.createToken(response);
 		return response;
 	}
-	public async updateMember(): Promise<string> {
-		console.log('Mutation:  updateMember');
-		return ' updateMember executed!';
+	public async updateMember(memberId: ObjectId, input: MemberUpdate): Promise<Member> {
+		const result: Member = await this.memberModel
+			.findOneAndUpdate(
+				{
+					_id: memberId,
+					memberStatus: MemberStatus.ACTIVE,
+				},
+				input,
+				{ new: true },
+			)
+			.exec();
+		if (!result) throw new InternalServerErrorException(Message.UPDATE_FAILED);
+
+		result.accsessToken = await this.authService.createToken(result);
+		return result;
 	}
 	public async getMember(): Promise<string> {
 		console.log('Query:  getMember');
