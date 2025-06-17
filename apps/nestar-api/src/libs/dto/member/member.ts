4421c9ca3c8693056520055a7ed6be3b5169b693
ika0109 <ikhboloy97@gmail.com>
2025-06-17 18:09:35 +0900

    develop getAgents GraphQl Api

--- apps/nestar-api/src/libs/dto/member/member.ts
+++ apps/nestar-api/src/libs/dto/member/member.ts
@@ -82,3 +82,18 @@ export class Member {
 	@Field(() => String, { nullable: true })
 	accsessToken?: string;
 }
+
+@ObjectType()
+export class TotalCounter {
+	@Field(() => Int, { nullable: true })
+	total: number;
+}
+
+@ObjectType()
+export class Members {
+	@Field(() => [Member])
+	list: Member[];
+
+	@Field(() => [TotalCounter], { nullable: true })
+	metaCounter: TotalCounter[];
+}
