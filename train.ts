09a945ac9a56ab21261d112778760931d0bbf171
ika0109 <ikhboloy97@gmail.com>
2025-06-17 19:34:48 +0900

    feat: MIT TASK ZO

--- train.ts
+++ train.ts
@@ -1,7 +1,25 @@
-function rotateArray(arr, index) {
-	return arr.slice(index + 1).concat(arr.slice(0, index + 1));
+function areParenthesesBalanced(str) {
+	let count = 0;
+
+	for (let char of str) {
+		if (char === '(') count++;
+		else if (char === ')') count--;
+
+		if (count < 0) return false;
+	}
+
+	return count === 0;
 }
-console.log(rotateArray([1, 2, 3, 4, 5, 6], 3));
+
+console.log(areParenthesesBalanced('string()ichida(qavslar)soni()balansda'));
+console.log(areParenthesesBalanced('string((ichida)qavslar)'));
+console.log(areParenthesesBalanced('string(ichida(qavslar)'));
+console.log(areParenthesesBalanced(')('));
+
+// / function rotateArray(arr, index) {
+// 	return arr.slice(index + 1).concat(arr.slice(0, index + 1));
+// }
+// console.log(rotateArray([1, 2, 3, 4, 5, 6], 3));
 
 // function satrniKebabga(satr) {
 // 	return satr.toLowerCase().replaceAll(' ', '-');
